df907d3bd00d8e594902de5bbbd51eb7e50d2c9a
nkpro2000 <45198561+nkpro2000@users.noreply.github.com>
2021-08-10 01:21:15 +0530

    Add load_weights
    i missed it. :facepalm:

--- model.py
+++ model.py
@@ -91,6 +91,21 @@ def get_model():
     
     return model
 
+def load_weights(model, yolo_weight_file):
+    data = np.fromfile(yolo_weight_file, np.float32)
+    data = data[4:]
+
+    index = 0
+    for layer in model.layers:
+        shape = [w.shape for w in layer.get_weights()]
+        if shape != []:
+            kshape, bshape = shape
+            bia = data[index:index + np.prod(bshape)].reshape(bshape)
+            index += np.prod(bshape)
+            ker = data[index:index + np.prod(kshape)].reshape(kshape)
+            index += np.prod(kshape)
+            layer.set_weights([ker, bia])
+
 
 class Box:
     def __init__(self):
@@ -196,7 +211,7 @@ def model_output_to_boxes(yolo_output, threshold=0.2, sqrt=1.8, C=20, B=2, S=7) 
 # Usage:
 """
 >>> model = get_model()
->>> model.load_weights('model.weights')
+>>> load_weights(model, 'model.weights')
 
 >>> img = mpimg.imread('testimg.jpg')
 
